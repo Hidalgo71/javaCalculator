be29b5f6cdc25d8e529c5fdd67b72dfa3f9727b0
Hidalgo71 <y.ozdemir71@gmail.com>
2019-11-23 01:39:11 +0300

    Java Calculator

--- src/calc.java
+++ src/calc.java
@@ -3,27 +3,27 @@ import javax.swing.*;
 public class calc {
     public static void main(String[] args) {
         JFrame frame = new JFrame("calc");
-        frame.setContentPane(new calc().kk);
+        frame.setContentPane(new calc().calculator);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
-    private JPanel kk;
+    private JPanel calculator;
     private JButton button2;
     private JButton button5;
-    private JTextField textField1;
+    private JTextField screen;
     private JButton button3;
     private JButton button4;
     private JButton button7;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
-    private JButton x2Button;
-    private JButton cButton;
+    private JButton buttonPlus;
+    private JButton buttonMinus;
+    private JButton buttonMulti;
+    private JButton button8;
+    private JButton buttonDivide;
+    private JButton buttonRemainder;
+    private JButton ButtonPower;
+    private JButton ButtonC;
     private JButton button9;
     private JButton button1;
     private JButton button6;
