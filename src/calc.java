45230bc3f188f72b8080ba68c28894585c4b8bc7
Hidalgo71 <y.ozdemir71@gmail.com>
2019-11-23 01:35:24 +0300

    Java Calculator

--- src/calc.java
+++ src/calc.java
@@ -10,22 +10,21 @@ public class calc {
     }
 
     private JPanel kk;
-    private JButton button1;
     private JButton button2;
     private JButton button5;
-    private JButton button6;
     private JTextField textField1;
     private JButton button3;
     private JButton button4;
     private JButton button7;
-    private JButton button9;
     private JButton button10;
     private JButton button11;
     private JButton button12;
     private JButton button13;
     private JButton button14;
     private JButton button15;
-    private JButton button16;
-    private JButton button8;
-    private JButton button17;
+    private JButton x2Button;
+    private JButton cButton;
+    private JButton button9;
+    private JButton button1;
+    private JButton button6;
 }
