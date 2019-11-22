8aec55dddfd4ee62fe592ed9c96ac39c1acf15f7
Hidalgo71 <y.ozdemir71@gmail.com>
2019-11-23 01:15:44 +0300

    Java Calculator

--- src/calc.java
+++ src/calc.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class calc {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("calc");
+        frame.setContentPane(new calc().kk);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JPanel kk;
 }
