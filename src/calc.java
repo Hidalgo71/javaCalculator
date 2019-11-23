1541062dce819a80b513a018007d6689c548cddf
Hidalgo71 <y.ozdemir71@gmail.com>
2019-11-23 13:27:05 +0300

    Java Calculator

--- src/calc.java
+++ src/calc.java
@@ -1,6 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class calc {
+    public calc() {
+        button0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+               String button0Text = screen.getText() + button0.getText();
+               screen.setText(button0Text);
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button1Text = screen.getText() + button1.getText();
+                screen.setText(button1Text);
+            }
+        });
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("calc");
         frame.setContentPane(new calc().calculator);
@@ -10,23 +30,23 @@ public class calc {
     }
 
     private JPanel calculator;
-    private JButton a2Button;
-    private JButton a5Button;
+    private JButton button2;
+    private JButton button5;
     private JTextField screen;
-    private JButton a3Button;
-    private JButton a4Button;
-    private JButton a7Button;
+    private JButton button3;
+    private JButton button4;
+    private JButton button7;
     private JButton buttonPlus;
     private JButton buttonMinus;
     private JButton buttonMulti;
     private JButton button8;
     private JButton buttonDivide;
     private JButton buttonRemainder;
-    private JButton ButtonPower;
-    private JButton ButtonC;
-    private JButton a9Button;
-    private JButton a1Button;
-    private JButton a6Button;
+    private JButton buttonPower;
+    private JButton buttonC;
+    private JButton button9;
+    private JButton button1;
+    private JButton button6;
     private JButton button0;
     private JButton buttonEqual;
 }
